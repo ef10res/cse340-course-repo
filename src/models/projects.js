4b3ed73d98d91b57713a9e3ee200abe1ba03ca62
ef10res <floreserick10@gmail.com>
2026-05-27 05:04:02 -0600

    organization

--- src/models/projects.js
+++ src/models/projects.js
@@ -27,3 +27,26 @@ export async function getAllProjects() {
     }
 }
 
+const getProjectsByOrganizationId = async (organizationId) => {
+    const query = `
+        SELECT
+          project_id,
+          organization_id,
+          title,
+          description,
+          location,
+          project_date
+        FROM service_project
+        WHERE organization_id = $1
+        ORDER BY project_date;
+      `;
+
+    const queryParams = [organizationId];
+    const result = await db.query(query, queryParams);
+
+    return result.rows;
+};
+
+// Export the model functions
+export { getProjectsByOrganizationId };
+
